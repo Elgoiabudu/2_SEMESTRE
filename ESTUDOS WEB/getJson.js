2557a3242410129aa66e300ffb7f571eb9251046
Fernando Bonde Leonelli <117871790+Elgoiabudu@users.noreply.github.com>
2023-05-04 12:48:17 -0300

    Implementação do verificador de CEP

--- ESTUDOS WEB/getJson.js
+++ ESTUDOS WEB/getJson.js
@@ -1,4 +1,4 @@
-function getJson() {
+function getJson(cep) {
             
     var cep = document.getElementById("cep").value;
        
@@ -7,17 +7,20 @@ function getJson() {
         var rua = data.logradouro;
         var bairro = data.bairro;
         var cidade = data.localidade;
+        var estado = data.uf;
         var erro = data.erro;
         
-        if(erro == "true"){
+        if(erro){
             document.getElementById("rua").value = "";
             document.getElementById("bairro").value = "";
             document.getElementById("cidade").value = "";
+            document.getElementById("estado").value = "";
             alert("CEP INVALIDO!");
         }else{
             document.getElementById("rua").value = rua;
             document.getElementById("bairro").value = bairro;
             document.getElementById("cidade").value = cidade;
+            document.getElementById("estado").value = estado;
         }                
     });
                     
