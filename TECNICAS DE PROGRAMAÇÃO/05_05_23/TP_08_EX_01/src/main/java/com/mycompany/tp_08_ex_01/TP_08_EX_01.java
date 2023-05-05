6f14459a1dbed3e086eeb3ed2a476d4773500634
Elgoiabudu <fernandobondeleonelli@gmail.com>
2023-05-05 19:23:23 -0300

    Estudos JFrame

---
+++ TECNICAS DE PROGRAMAÇÃO/05_05_23/TP_08_EX_01/src/main/java/com/mycompany/tp_08_ex_01/TP_08_EX_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp_08_ex_01;
+
+/**
+ *
+ * @author labs
+ */
+public class TP_08_EX_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
