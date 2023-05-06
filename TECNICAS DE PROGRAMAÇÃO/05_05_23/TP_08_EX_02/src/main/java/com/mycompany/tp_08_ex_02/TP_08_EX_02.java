069353c22949c0aed136bc93f899522318396f3c
Elgoiabudu <fernandobondeleonelli@gmail.com>
2023-05-05 21:55:55 -0300

    Update TP_08_EX_02.java

--- TECNICAS DE PROGRAMAÇÃO/05_05_23/TP_08_EX_02/src/main/java/com/mycompany/tp_08_ex_02/TP_08_EX_02.java
+++ TECNICAS DE PROGRAMAÇÃO/05_05_23/TP_08_EX_02/src/main/java/com/mycompany/tp_08_ex_02/TP_08_EX_02.java
@@ -3,6 +3,7 @@
  */
 package com.mycompany.tp_08_ex_02;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
@@ -30,8 +31,7 @@ public class TP_08_EX_02 {
 
     private void montaTela() {
         preparaJanela();
-        mostraJanela();
-        preparaPainelPrincipal();
+        mostraJanela();        
         //preparaPainelLimpar();
         //preparaVisor();
         //btn1();
@@ -43,42 +43,49 @@ public class TP_08_EX_02 {
     private void preparaJanela() {
         janela = new JFrame("Teclado Celular");
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //setLayout(new BorderLayout());
+        
+        JButton btn1 = new JButton("1");
+        JButton btn2 = new JButton("1");
+        JButton btn3 = new JButton("1");
+        JButton btn4 = new JButton("1");
+        JButton btn5 = new JButton("1");
+        JButton btn6 = new JButton("1");
+        JButton btn7 = new JButton("1");
+        JButton btn8 = new JButton("1");
+        JButton btn9 = new JButton("1");
+        JButton btn10 = new JButton("1");
+        JButton btn11 = new JButton("1");
+        JButton btn12 = new JButton("1");
+        
+        
+        
+        painelPrincipal = new JPanel();
+        painelPrincipal.setLayout(new GridLayout(4,3)); 
+        janela.add(painelPrincipal, BorderLayout.CENTER);
+        
+        painelLimpar = new JPanel(new FlowLayout());        
+        janela.add(painelLimpar, BorderLayout.SOUTH);
+        
+        painelPrincipal.add(btn1);
+        painelPrincipal.add(btn2);
+        painelPrincipal.add(btn3);
+        painelPrincipal.add(btn4);
+        painelPrincipal.add(btn5);
+        painelPrincipal.add(btn6);
+        painelPrincipal.add(btn7);
+        painelPrincipal.add(btn8);
+        painelPrincipal.add(btn9);
+        painelPrincipal.add(btn10);
+        painelPrincipal.add(btn11);
+        painelPrincipal.add(btn12);
     }
 
     private void mostraJanela() {
         janela.pack();
         janela.setSize(600, 300);
         janela.setVisible(true);
-    }
-
-    private void preparaPainelPrincipal() {
-        painelPrincipal = new JPanel();        
-        painelPrincipal.setLayout(new GridLayout(4,3));        
-        //FlowLayout f = (FlowLayout) painelPrincipal.getLayout();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        btn1();
-        janela.add(painelPrincipal);   
-        
-    }
-    
-    private void preparaPainelLimpar() {
-        painelLimpar = new JPanel();        
-        //painelLimpar.setLayout(new GridLayout(1,1));    
-        
-        painelLimpar.setLayout(new BoxLayout(painelLimpar,BoxLayout.PAGE_AXIS));
-        //FlowLayout f = (FlowLayout) painelPrincipal.getLayout();
-        btn1();
-        janela.add(painelLimpar);
     }
 
     private void preparaVisor() {
@@ -92,33 +99,7 @@ public class TP_08_EX_02 {
         painelPrincipal.add(btn);//adiciona botão ao painel
     }
     
-    private void btn2() {
-        JButton btn = new JButton("2");
-        adicionar(btn);       
-        painelPrincipal.add(btn);//adiciona botão ao painel
-    }
     
-    private void btn3() {
-        JButton btn = new JButton("1");
-        adicionar(btn);
-        painelPrincipal.setLocation(50, 100);//posição do painel no frame 
-        painelPrincipal.add(btn);//adiciona botão ao painel
-    }
-    
-    private void btn4() {
-        JButton btn = new JButton("1");
-        adicionar(btn);
-        painelPrincipal.setLocation(50, 100);//posição do painel no frame 
-        painelPrincipal.add(btn);//adiciona botão ao painel
-    }
-    
-    private void btn5() {
-        JButton btn = new JButton("1");
-        adicionar(btn);
-        painelPrincipal.setLocation(50, 100);//posição do painel no frame 
-        painelPrincipal.add(btn);//adiciona botão ao painel
-    }
-
     private void adicionar(JButton btn) {
         btn.addActionListener(new ActionListener() {
 
@@ -129,6 +110,4 @@ public class TP_08_EX_02 {
         });
 
     }
-
-    
 }
