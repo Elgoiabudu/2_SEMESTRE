e25db7016f28cbeed0734e9f8cfb193a8ea8bdf7
Fernando Bonde Leonelli <fernandobondeleonelli@gmail.com>
2023-05-07 14:31:47 -0300

    EX_02

---
+++ TECNICAS DE PROGRAMAÇÃO/05_05_23/TP_08_EX_02/TP_08_EX_02/src/main/java/com/mycompany/tp_08_ex_02/TP_08_EX_02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp_08_ex_02;
+
+/**
+ *
+ * @author Fernando B. Leonelli
+ */
+public class TP_08_EX_02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
